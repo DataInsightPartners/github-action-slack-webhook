ca976670280248bf646b3e421e1a2ba71d2969d7
Justin W <justinwhite15@gmail.com>
2020-04-07 23:11:41 -0700

    Format updating

--- index.js
+++ index.js
@@ -11,8 +11,9 @@ const runId = process.env.GITHUB_RUN_ID; // actions/runs/
 
 async function run() {
   const url = core.getInput('slack-webhook-url', { required: true });
+  
   var arguments = {
-    username: 'FAFSA Tracker: CI/CD',
+    username: context.repo.repo + ':' + context.workflow,
     icon_emoji: ':+1',
     channel: '#devops'
   };
@@ -23,29 +24,29 @@ async function run() {
   var fields = [];
   fields.push({
     "type": "mrkdwn",
-    "text": "*Event:*\n" + context.eventName + "\n"
+    "text": "*Event:*\n" + context.eventName
   });
 
   fields.push({
     "type": "mrkdwn",
-    "text": "*Ref:*\n" + context.ref + "\n"
+    "text": "*Ref:*\n" + context.ref
   });
 
   if(context.eventName === 'pull_request') {
     fields.push({
       "type": "mrkdwn",
-      "text": "*PR:*\n<" + context.payload.pull_request.html_url + "|#" + context.payload.pull_request.number + "> - " + context.payload.pull_request.body.substring(0,30) + "\n"
+      "text": "*PR:*\n<" + context.payload.pull_request.html_url + "|#" + context.payload.pull_request.number + "> - " + context.payload.pull_request.body.substring(0,30)
     });
   }
 
   fields.push({
     "type": "mrkdwn",
-    "text": "*Actor:*\n" + context.actor + "\n"
+    "text": "*Actor:*\n" + context.actor
   });
 
   fields.push({
     "type": "mrkdwn",
-    "text": "*Commit:*\n<https://github.com/" + repo_path + "/commit/" + context.sha + "|" + context.sha.substring(0,7) + ">\n"
+    "text": "*Commit:*\n<https://github.com/" + repo_path + "/commit/" + context.sha + "|" + context.sha.substring(0,7) + ">"
   });
 
   
@@ -55,7 +56,7 @@ async function run() {
         "type": "section",
         "text": {
           "type": "mrkdwn",
-          "text": "*" + repo_path + "*\nJob Run Succeeded: *<https://github.com/" + repo_path + "/actions/runs/" + runId + "|" + context.workflow + ">*"
+          "text": "Job Run Succeeded: *<https://github.com/" + repo_path + "/actions/runs/" + runId + "|" + context.workflow + ">*"
         }
       },
       {
