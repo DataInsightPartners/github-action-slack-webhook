05e4e3d82b822be672e0779e9fd70732dc69811e
Justin W <justinwhite15@gmail.com>
2020-04-07 22:14:48 -0700

    Update and Repackage

--- index.js
+++ index.js
@@ -1,52 +1,65 @@
 const core = require('@actions/core');
 const { IncomingWebhook } = require('@slack/webhook');
 
+const repository = process.env.GITHUB_REPOSITORY;
+const event = process.env.GITHUB_EVENT_NAME;
+const runId = process.env.GITHUB_RUN_ID;
+const commit = process.env.GITHUB_SHA;
+
+
+
+
 async function run() {
   const url = core.getInput('slack-webhook-url', { required: true });
   const webhook = new IncomingWebhook(url);
   
-  try{
-    await webhook.send({
-      "blocks": [
-        {
-          "type": "section",
-          "text": {
-            "type": "mrkdwn",
-            "text": "Job Run Succeeded: *<https://github.com/DataInsightPartners/FAFSA-Laravel-App/runs/569589487|test>*"
-          }
-        },
-        {
-          "type": "divider"
-        },
-        {
-          "type": "section",
-          "fields": [
-            {
-              "type": "mrkdwn",
-              "text": "*PR:*\n<https://github.com/DataInsightPartners/MyEducationData/pull/3028|#3130> - Fflv Aggregation Final"
-            },
-                            {
-              "type": "mrkdwn",
-              "text": "*Branch:*\nfflv-aggregation"
-            },
-                            {
-              "type": "mrkdwn",
-              "text": "*Author:*\nBen Robinson"
-            },
-                            {
-              "type": "mrkdwn",
-              "text": "*Commit:*\n<https://github.com/DataInsightPartners/MyEducationData/pull/3028/commits/8a132d4843e537de9b5367d92535fb22ab3f2f30|8a132d4>"
-            }
-          ]
+  
+  var arguments = {
+    username: 'FAFSA Tracker: CI/CD',
+    icon_emoji: ':+1',
+    channel: '#devops'
+  };
+  
+  var message = {
+    "blocks": [
+      {
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": "Job Run Succeeded: *<https://github.com/" + repository + "/runs/" + runId + "|test>*"
         }
-      ]
-    }
-    ,
-    {
-      username: 'FAFSA Tracker: CI/CD',
-      icon_emoji: ':+1',
-      channel: '#devops'
-    });
+      },
+      {
+        "type": "divider"
+      },
+      {
+        "type": "section",
+        "fields": [
+          {
+            "type": "mrkdwn",
+            "text": "*PR:*\n<https://github.com/" + repository + "/pull/3028|#3130> - Fflv Aggregation Final"
+          },
+                          {
+            "type": "mrkdwn",
+            "text": "*Branch:*\nfflv-aggregation"
+          },
+                          {
+            "type": "mrkdwn",
+            "text": "*Author:*\nBen Robinson"
+          },
+                          {
+            "type": "mrkdwn",
+            "text": "*Commit:*\n<https://github.com/" + repository + "/commits/" + commit + "|" + commit.slice(7) + ">"
+          }
+        ]
+      }
+    ]
+  };
+  
+  
+  // Send Webhook Post Request
+  try{
+    await webhook.send(message, arguments);
   } catch (error) {
     core.setFailed(`Action failed with error ${error}`);
   }
